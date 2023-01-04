e225185b973af51349f87b6dbce00c8502fbbd86
Harsh Thakkar <hthakkar@glgroup.com>
2023-01-04 12:28:22 +0530

    add database objects

---
+++ Db_objects.sql
@@ -0,0 +1,509 @@
+DROP TABLE IF EXISTS dbo.[Product_Subproduct_relation]
+DROP TABLE IF EXISTS dbo.[Subproduct_RM_usage]
+DROP TABLE IF EXISTS dbo.[Product_RM_usage]
+DROP TABLE IF EXISTS dbo.[Raw_material_supplier_relation]
+DROP TABLE IF EXISTS dbo.[Supplier]
+DROP TABLE IF EXISTS dbo.Payment
+DROP TABLE IF EXISTS dbo.[Transaction_details]
+DROP TABLE IF EXISTS dbo.[Sub_Product]
+DROP TABLE IF EXISTS dbo.[Raw_Material]
+DROP TABLE IF EXISTS dbo.[Rawmaterial_Category]
+DROP TABLE IF EXISTS dbo.[Measurement_unit]
+DROP TABLE IF EXISTS dbo.[Transaction_History] 
+DROP TABLE IF EXISTS dbo.[Transaction] 
+DROP TABLE IF EXISTS dbo.[Product]
+DROP TABLE IF EXISTS dbo.[Location]
+DROP TABLE IF EXISTS dbo.[Member]
+DROP TABLE IF EXISTS dbo.[Memeber_Category] 
+DROP TABLE IF EXISTS dbo.[Product_Category] 
+
+
+DROP TABLE IF EXISTS dbo.[Product_Category] 
+CREATE TABLE dbo.[Product_Category] (
+  [category_id]  INT IDENTITY(1,1),
+  [Name] varchar(30) NOT NULL,
+  [Description] varchar(400)  NULL,
+  PRIMARY KEY ([category_id])
+);
+
+INSERT INTO dbo.Product_Category (name,[Description]) VALUES ('Toran','Bandhdi toran')
+
+DROP TABLE IF EXISTS dbo.[Memeber_Category] 
+
+CREATE TABLE dbo.[Memeber_Category] (
+  [member_category_id] INT IDENTITY(1,1),
+  [Name] varchar(30) NOT NULL,
+  [Description] varchar(400) NULL,
+  PRIMARY KEY ([member_category_id])
+);
+
+INSERT INTO dbo.[Memeber_Category] (name,[Description]) VALUES ('jeko moti','jeko moti')
+
+
+DROP TABLE IF EXISTS dbo.[Member]
+CREATE TABLE dbo.[Member] (
+  [Member_id] INT IDENTITY(1,1),
+  [F_Name] varchar(30) NOT NULL,
+  [L_Name] varchar(30) NULL,
+  [Contact_no] Varchar(10) NOT NULL,
+  [Alternate_conatact_no] Varchar(10) NULL,
+  [Address] VARCHAR(4000) NULL,
+  [Aadhar] varchar(20)  NULL,
+  [Deposit Amount] INT NOT NULL,
+  [Status] BIT NOT NULL,
+  [Created_at] DATETIME NOT NULL ,
+  [Updated_at] DATETIME NOT NULL,
+  [member_Category_id] INT NOT NULL,
+  PRIMARY KEY ([Member_id]),
+  CONSTRAINT [FK_Member.member_Category_id]
+    FOREIGN KEY ([member_Category_id])
+      REFERENCES [Memeber_Category]([member_category_id])
+);
+
+ALTER TABLE dbo.Member ADD CONSTRAINT DF_Member_Created_at  DEFAULT GETDATE() FOR Created_at 
+ALTER TABLE dbo.Member ADD CONSTRAINT DF_Member_Updated_at  DEFAULT GETDATE() FOR Updated_at
+ALTER TABLE dbo.Member ADD CONSTRAINT DF_Member_Status  DEFAULT 1 FOR [Status]
+
+INSERT INTO dbo.Member
+(
+    F_Name,
+    L_Name,
+    Contact_no,
+    Alternate_conatact_no,
+    Address,
+    AADHAR,
+    [Deposit Amount],
+    member_Category_id
+)
+VALUES
+(   'xyz', -- F_Name - varchar(30)
+    'Thakkar', -- L_Name - varchar(30)
+    1234567890, -- Contact_no - int
+    1234567890, -- Alternate_conatact_no - int
+    'ngdkakl', -- Address - varchar(400)
+    NULL, -- AADHAR - varchar(20)
+    500, -- Deposit Amount - int
+    1  -- member_Category_id - int
+    )
+
+DROP TABLE IF EXISTS dbo.[Location]
+CREATE TABLE dbo.[Location] (
+  [Location_id] INT IDENTITY(1,1),
+  [Name] varchar(30) NOT NULL,
+  [Rack] INT NOT NULL,
+  PRIMARY KEY ([Location_id])
+);
+
+INSERT INTO dbo.[Location] VALUES ('A',1)
+
+DROP TABLE IF EXISTS dbo.[Product]
+CREATE TABLE dbo.[Product] (
+  [Product_Id] INT IDENTITY(1,1),
+  [Name] varchar(30) NOT NULL,
+  [Description] varchar(400)  NULL,
+  [SKU] INT NOT NULL,
+  [Created_at] DATETIME NOT NULL,
+  [Updated_at] DATETIME NOT NULL,
+  [Active] BIT NOT NULL,
+  [Category_id] INT NOT NULL,
+  [Location_id] INT NOT NULL,
+  [image_url] VARBINARY NULL,
+  [Labor Rate] INT NOT NULL,
+  PRIMARY KEY ([Product_Id]),
+  CONSTRAINT [FK_Product.Category_id]
+    FOREIGN KEY ([Category_id])
+      REFERENCES [Product_Category]([category_id]),
+  CONSTRAINT [FK_Product.Location_id]
+    FOREIGN KEY ([Location_id])
+      REFERENCES [Location]([Location_id])
+);
+
+ALTER TABLE dbo.[Product] ADD CONSTRAINT DF_Product_Created_at  DEFAULT GETDATE() FOR [Created_at] 
+ALTER TABLE dbo.[Product] ADD CONSTRAINT DF_Product_Updated_at  DEFAULT GETDATE() FOR [Updated_at]
+ALTER TABLE dbo.[Product] ADD CONSTRAINT DF_Product_Active  DEFAULT 1 FOR [Active]
+
+INSERT INTO dbo.Product
+(
+    Name,
+    Description,
+    SKU,
+    Active,
+    Category_id,
+    Location_id,
+    image_url,
+    [Labor Rate]
+)
+VALUES
+(   'PT-1207 Toran', -- Name - varchar(30)
+    NULL, -- Description - varchar(400)
+    10, -- SKU - int
+    1, -- Active - bit
+    1, -- Category_id - int
+    1, -- Location_id - int
+    NULL, -- image_url - varbinary
+    15  -- Labor Rate - int
+    )
+
+	--SELECT * FROM dbo.Product p
+	--JOIN product_rm_usage r ON r.product_id =p.product_id
+	--JOIN raw_material rm ON rm.rawmaterial_id=r.rawmaterial_id
+
+DROP TABLE IF EXISTS dbo.[Transaction] 
+CREATE TABLE dbo.[Transaction] (
+  [Trx_id] INT IDENTITY(1,1),
+  [Member_id] INT NOT NULL,
+  [Product_id] INT   NULL,
+  [subproduct_id] INT  NULL,
+  [Required_Qty] INT  NOT NULL,
+  [Total_Weight given] decimal(18,2)  NOT NULL,
+  [Total_weight_recieved] decimal(18,2)  NULL,
+  [Recieved_qty] INT NULL,
+  [Order_datte] DATETIME  NOT NULL,
+  [Return_date] DATETIME  NOT NULL,
+  [Actual_return_date] DATETIME NULL,
+  [Valid_From] DATETIME  NOT NULL,
+  [Valide_to] DATETIME  NOT NULL,
+  [Is_completed] BIT NOT NULL,
+  [Calculated_Piece_Labor_Rate] DECIMAL(18,2) NOT NULL,
+  [Actual_piece_labor rate] DECIMAL (18,2) NOT NULL,
+  [Total_Actual_Amount] decimal(18,2) NOT NULL,
+  PRIMARY KEY ([Trx_id]),
+  CONSTRAINT [FK_Transaction.Member_id]
+    FOREIGN KEY ([Member_id])
+      REFERENCES [Member]([Member_id]),
+  CONSTRAINT [FK_Transaction.Product_id]
+    FOREIGN KEY ([Product_id])
+      REFERENCES [Product]([Product_Id])
+);
+
+ALTER TABLE dbo.[Transaction] ADD CONSTRAINT DF_Transaction_Valid_from  DEFAULT GETDATE() FOR [Valid_from] 
+ALTER TABLE dbo.[Transaction] ADD CONSTRAINT DF_Transaction_Valid_to  DEFAULT GETDATE() FOR [Valide_to] 
+
+
+DROP TABLE IF EXISTS dbo.[Transaction_History] 
+CREATE TABLE dbo.[Transaction_History] (
+  [Trx_id] INT IDENTITY(1,1),
+  [Member_id] INT NOT NULL,
+  [Product_id] INT  NULL,
+  [subproduct_id] INT NULL,
+  [Required_Qty] INT NOT NULL,
+  [Total_Weight given] decimal(18,2) NOT NULL,
+  [Total_weight_recieved] decimal(18,2) NULL,
+  [Recieved_qty] INT NULL,
+  [Order_datte] DATETIME NOT NULL,
+  [Return_date] DATETIME NOT NULL,
+  [Actual_return_date] DATETIME NULL,
+  [Is_completed] BIT NOT NULL,
+  [Calculated_Piece_Labor_Rate] DECIMAL(18,2) NOT NULL,
+  [Actual_piece_labor rate] DECIMAL(18,2)  NOT NULL,
+  [Total_Actual Amount] decimal(18,2)  NOT NULL,
+  [Valid_From] DATETIME  NOT NULL,
+  [Valid_to] DATETIME  NOT NULL,
+  CONSTRAINT [FK_Transaction_History.Trx_id]
+    FOREIGN KEY ([Trx_id])
+      REFERENCES [Transaction]([Trx_id])
+);
+
+--ALTER TABLE dbo.[Transaction_History] ADD CONSTRAINT DF_Transaction_History_Created_at  DEFAULT GETDATE() FOR [Created_at] 
+--ALTER TABLE dbo.[Transaction_History] ADD CONSTRAINT DF_Transaction_History_Updated_at  DEFAULT GETDATE() FOR [Updated_at] 
+
+DROP TABLE IF EXISTS dbo.[Measurement_unit]
+CREATE TABLE dbo.[Measurement_unit] (
+  [unit_id] INT IDENTITY(1,1),
+  [unit_type] varchar(30) NOT NULL,
+  PRIMARY KEY ([unit_id])
+);
+
+INSERT INTO dbo.Measurement_unit
+(
+    unit_type
+)
+VALUES
+('kg' -- unit_type - varchar(30)
+    ),('Piece')
+
+DROP TABLE IF EXISTS dbo.[Rawmaterial_Category]
+CREATE TABLE dbo.[Rawmaterial_Category] (
+  [category_id] INT IDENTITY(1,1),
+  [Name] varchar(30) NOT NULL,
+  [Description] varchar(400) NULL,
+  PRIMARY KEY ([category_id])
+);
+
+INSERT INTO dbo.[Rawmaterial_Category] 
+VALUES (
+'default','default')
+
+DROP TABLE IF EXISTS dbo.[Raw_Material]
+CREATE TABLE dbo.[Raw_Material] (
+  [Rawmaterial_Id] INT IDENTITY(1,1),
+  [Name] varchar(30) NOT NULL,
+  [Description] varchar(400) NULL,
+  [SKU] decimal(18,2) NOT NULL,
+  [unit_id] INT NOT NULL,
+  [Created_at] DATETIME NOT NULL,
+  [Updated_at] DATETIME NOT NULL,
+  [Active] BIT NOT NULL,
+  [Category_id] INT NOT NULL,
+  [Location_id] INT NOT NULL,
+  [image_url] VARBINARY  NULL,
+  PRIMARY KEY ([Rawmaterial_Id]),
+  CONSTRAINT [FK_Raw_Material.Rawmaterial_Id]
+    FOREIGN KEY (unit_id)
+      REFERENCES [Measurement_unit]([unit_id]),
+  CONSTRAINT [FK_Raw_Material.Location_id]
+    FOREIGN KEY ([Location_id])
+      REFERENCES [Location]([Location_id]),
+  CONSTRAINT [FK_Raw_Material.Category_id- Remove]
+    FOREIGN KEY ([Category_id])
+      REFERENCES [Rawmaterial_Category]([category_id])
+);
+
+ALTER TABLE dbo.[Raw_Material] ADD CONSTRAINT DF_Raw_material_Product_Created_at  DEFAULT GETDATE() FOR [Created_at] 
+ALTER TABLE dbo.[Raw_Material] ADD CONSTRAINT DF_Raw_material_Product_Updated_at  DEFAULT GETDATE() FOR [Updated_at]
+ALTER TABLE dbo.[Raw_Material] ADD CONSTRAINT DF_Raw_material_Product_Active DEFAULT 1 FOR Active
+
+INSERT INTO dbo.Raw_Material
+(
+    Name,
+    Description,
+    SKU,
+    unit_id,
+    Active,
+    Category_id,
+    Location_id,
+    image_url
+)
+VALUES
+(   '30mm beads', -- Name - varchar(30)
+    NULL, -- Description - varchar(400)
+    5000, -- SKU - decimal(18, 2)
+    1, -- unit_id - int
+    1, -- Active - bit
+    1, -- Category_id - int
+    1, -- Location_id - int
+    NULL  -- image_url - varbinary
+    ),( '8mm beads', -- Name - varchar(30)
+    NULL, -- Description - varchar(400)
+    4000, -- SKU - decimal(18, 2)
+    1, -- unit_id - int
+    1, -- Active - bit
+    1, -- Category_id - int
+    1, -- Location_id - int
+    NULL  -- image_url - varbinary
+	),
+	( 'steel patti', -- Name - varchar(30)
+    NULL, -- Description - varchar(400)
+    50, -- SKU - decimal(18, 2)
+    2, -- unit_id - int
+    1, -- Active - bit
+    1, -- Category_id - int
+    1, -- Location_id - int
+    NULL  -- image_url - varbinary
+	),('14 mm Fruitball', -- Name - varchar(30)
+    NULL, -- Description - varchar(400)
+    5058, -- SKU - decimal(18, 2)
+    1, -- unit_id - int
+    1, -- Active - bit
+    1, -- Category_id - int
+    1, -- Location_id - int
+    NULL  -- image_url - varbinary
+	)
+
+
+	--SELECT * FROM dbo.Raw_Material
+
+DROP TABLE IF EXISTS dbo.[Sub_Product]
+CREATE TABLE dbo.[Sub_Product] (
+  [SubProduct_Id] INT IDENTITY(1,1),
+  [Name] varchar(30) NOT NULL,
+  [Description] varchar(400) NULL,
+  [SKU] INT NOT NULL,
+  [Created_at] DATETIME NOT NULL,
+  [Updated_at] DATETIME NOT NULL,
+  [Active] BIT NOT NULL,
+  [Location_id] INT NOT NULL,
+  [image_url] VARBINARY NULL,
+  [Labour rate] INT NOT NULL,
+  PRIMARY KEY ([SubProduct_Id])
+);
+
+ALTER TABLE dbo.[Sub_Product] ADD CONSTRAINT DF_Sub_Product_Created_at  DEFAULT GETDATE() FOR [Created_at] 
+ALTER TABLE dbo.[Sub_Product] ADD CONSTRAINT DF_Sub_Product_Updated_at  DEFAULT GETDATE() FOR [Updated_at]
+ALTER TABLE dbo.[Sub_Product] ADD CONSTRAINT DF_Sub_Product_Active DEFAULT 1 FOR Active
+
+INSERT INTO dbo.Sub_Product
+(
+    Name,
+    Description,
+    SKU,
+    Created_at,
+    Updated_at,
+    Location_id,
+    image_url,
+    [Labour rate]
+)
+VALUES
+(   'Madhela Katha Small',        -- Name - varchar(30)
+    NULL,      -- Description - varchar(400)
+    0,         -- SKU - int
+    GETDATE(), -- Created_at - datetime
+    GETDATE(), -- Updated_at - datetime
+    0,         -- Location_id - int
+    NULL,      -- image_url - varbinary
+    2          -- Labour rate - int
+    )
+
+DROP TABLE IF EXISTS dbo.[Transaction_details]
+CREATE TABLE dbo.[Transaction_details] (
+  [Trxdetails_id] INT IDENTITY(1,1),
+  [Trx_id] INT NOT NULL,
+  [Rawmaterial_id] INT NOT NULL,
+  [subproduct_id] INT NOT NULL,
+  [Required_qty] decimal(18,2) NOT NULL,
+  [Given_qty] decimal(18,2) NOT NULL,
+  [Is_pending] BIT NOT NULL,
+  [created_at] DATETIME NOT NULL,
+  [Updated_at] DATETIME NOT NULL,
+  PRIMARY KEY ([Trxdetails_id]),
+  CONSTRAINT [FK_Transaction_details.Trxdetails_id]
+    FOREIGN KEY ([Trxdetails_id])
+      REFERENCES [Transaction]([Trx_id]),
+  CONSTRAINT [FK_Transaction_details.Rawmaterial_id]
+    FOREIGN KEY ([Rawmaterial_id])
+      REFERENCES [Raw_Material]([Rawmaterial_Id]),
+  CONSTRAINT [FK_Transaction_details.subproduct_id]
+    FOREIGN KEY ([subproduct_id])
+      REFERENCES [Sub_Product]([SubProduct_Id])
+);
+
+ALTER TABLE dbo.[Transaction_details] ADD CONSTRAINT DF_Transaction_details_Created_at  DEFAULT GETDATE() FOR [Created_at] 
+ALTER TABLE dbo.[Transaction_details] ADD CONSTRAINT DF_Transaction_details_Updated_at  DEFAULT GETDATE() FOR [Updated_at]
+
+
+DROP TABLE IF EXISTS dbo.Payment
+CREATE TABLE dbo.[Payment] (
+  [Payment_id] INT IDENTITY(1,1),
+  [Member_id] INT NOT NULL,
+  [Trx_id] INT NOT NULL,
+  [Payment_date] INT NOT NULL,
+  [Mode] INT NOT NULL,
+  [created_at] varchar(20) NOT NULL,
+  [updated_at] varchar(20) NOT NULL,
+  PRIMARY KEY ([Payment_id]),
+  CONSTRAINT [FK_Payment.Trx_id]
+    FOREIGN KEY ([Trx_id])
+      REFERENCES [Transaction]([Trx_id]),
+  CONSTRAINT [FK_Payment.Member_id]
+    FOREIGN KEY ([Member_id])
+      REFERENCES [Member]([Member_id])
+);
+
+ALTER TABLE dbo.[Payment] ADD CONSTRAINT DF_Payment_Created_at  DEFAULT GETDATE() FOR [Created_at] 
+ALTER TABLE dbo.[Payment] ADD CONSTRAINT DF_Payment_Updated_at  DEFAULT GETDATE() FOR [Updated_at]
+
+
+DROP TABLE IF EXISTS dbo.[Supplier]
+CREATE TABLE dbo.[Supplier] (
+  [supplier_id] INT IDENTITY(1,1),
+  [Name] varchar(30) NOT NULL,
+  [Contact_Person] VARCHAR(30) NULL,
+  [Description] varchar(400) NULL,
+  [Contact] VARCHAR(10) NULL,
+  [city] VARCHAR(20) NOT NULL,
+  PRIMARY KEY ([supplier_id])
+);
+
+DROP TABLE IF EXISTS dbo.[Raw_material_supplier_relation]
+CREATE TABLE dbo.[Raw_material_supplier_relation] (
+  [RMS_id] INT IDENTITY(1,1),
+  [supplier_id] INT NOT NULL,
+  [Rawmaterial_Id] INT NOT NULL,
+  PRIMARY KEY ([RMS_id]),
+  CONSTRAINT [FK_Raw_material_supplier_relation.Rawmaterial_Id]
+    FOREIGN KEY ([Rawmaterial_Id])
+      REFERENCES [Raw_Material]([Rawmaterial_Id]),
+  CONSTRAINT [FK_Raw_material_supplier_relation.supplier_id]
+    FOREIGN KEY ([supplier_id])
+      REFERENCES [Supplier]([supplier_id])
+);
+
+DROP TABLE IF EXISTS dbo.[Product_RM_usage]
+CREATE TABLE dbo.[Product_RM_usage] (
+  [PRM_id] INT IDENTITY(1,1),
+  [Product_id] INT NOT NULL,
+  [Rawmaterial_id] INT NOT NULL,
+  [Required_qty] decimal(18,2) NOT NULL,
+  PRIMARY KEY ([PRM_id]),
+  CONSTRAINT [FK_Product_RM_usage.Product_id]
+    FOREIGN KEY ([Product_id])
+      REFERENCES [Product]([Product_Id]),
+  CONSTRAINT [FK_Product_RM_usage.Rawmaterial_id]
+    FOREIGN KEY ([Rawmaterial_id])
+      REFERENCES [Raw_Material]([Rawmaterial_Id])
+);
+
+INSERT INTO dbo.Product_RM_usage
+(
+    Product_id,
+    Rawmaterial_id,
+    Required_qty
+)
+VALUES
+(   1,   -- Product_id - int
+    1,   -- Rawmaterial_id - int
+    15 -- Required_qty - decimal(18, 2)
+    ),(  1,   -- Product_id - int
+    2,   -- Rawmaterial_id - int
+    420 -- Required_qty - decimal(18, 2)
+    ),(   1,   -- Product_id - int
+    3,   -- Rawmaterial_id - int
+    100 -- Required_qty - decimal(18, 2)
+    ),
+	( 1,   -- Product_id - int
+    4,   -- Rawmaterial_id - int
+    150 -- Required_qty - decimal(18, 2)
+    )
+
+DROP TABLE IF EXISTS dbo.[Subproduct_RM_usage]
+CREATE TABLE dbo.[Subproduct_RM_usage] (
+  [SPRM_id] INT IDENTITY(1,1),
+  [Subproduct_id] INT NOT NULL,
+  [Rawmaterial_id] INT NOT NULL,
+  [Required_qty] decimal(18,2) NOT NULL,
+  PRIMARY KEY ([SPRM_id]),
+  CONSTRAINT [FK_Subproduct_RM_usage.Subproduct_id]
+    FOREIGN KEY ([Subproduct_id])
+      REFERENCES [Sub_Product]([SubProduct_Id]),
+  CONSTRAINT [FK_Subproduct_RM_usage.Rawmaterial_id]
+    FOREIGN KEY ([Rawmaterial_id])
+      REFERENCES [Raw_Material]([Rawmaterial_Id])
+);
+
+
+DROP TABLE IF EXISTS dbo.[Product_Subproduct_relation]
+CREATE TABLE dbo.[Product_Subproduct_relation] (
+  [PSR_id] INT IDENTITY(1,1),
+  [Product_id] INT NOT NULL,
+  [SubProduct_id] INT NOT NULL,
+  [Required_qty] DECIMAL(18,2) NOT NULL,
+  PRIMARY KEY ([PSR_id]),
+  CONSTRAINT [FK_Product_Subproduct_relation.SubProduct_id]
+    FOREIGN KEY ([SubProduct_id])
+      REFERENCES [Sub_Product]([SubProduct_Id]),
+  CONSTRAINT [FK_Product_Subproduct_relation.Product_id]
+    FOREIGN KEY ([Product_id])
+      REFERENCES [Product]([Product_Id])
+);
+
+INSERT INTO dbo.Product_Subproduct_relation
+(
+    Product_id,
+    SubProduct_id,
+    Required_qty
+)
+VALUES
+(   1,   -- Product_id - int
+    1,   -- SubProduct_id - int
+    2 -- Required_qty - decimal(18, 2)
+    )
