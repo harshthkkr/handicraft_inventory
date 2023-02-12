c4a584ceeb6572e1dedb505e8352c37c72b7f1cf
Harsh Thakkar <72026921+harshthkkr@users.noreply.github.com>
2023-02-12 22:06:06 +0530

    Update TR_rm_update.sql

--- TR_rm_update.sql
+++ TR_rm_update.sql
@@ -11,4 +11,9 @@ SET SKU = SKU - i.given_qty
 FROM inserted i
 WHERE Raw_Material.Rawmaterial_Id = i.Rawmaterial_Id ;
 
+UPDATE dbo.Sub_Product
+SET SKU = SKU - i.given_qty 
+FROM inserted i
+WHERE Sub_Product.SubProduct_Id = i.SubProduct_Id ;
+
 END;
